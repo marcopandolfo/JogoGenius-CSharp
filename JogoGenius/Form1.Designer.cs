5cd0b969b605eadd43155821ea432e36721c4e50
Marco Antonio <marcopandolfo577@gmail.com>
2018-11-02 17:25:22 -0200

    Criado

---
+++ JogoGenius/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace JogoGenius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lblPontos = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkRed;
+            this.pictureBox1.Location = new System.Drawing.Point(81, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 86);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "R";
+            this.pictureBox1.Click += new System.EventHandler(this.Clique);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(189, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 86);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "G";
+            this.pictureBox2.Click += new System.EventHandler(this.Clique);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox3.Location = new System.Drawing.Point(189, 165);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 86);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Y";
+            this.pictureBox3.Click += new System.EventHandler(this.Clique);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.MediumBlue;
+            this.pictureBox4.Location = new System.Drawing.Point(81, 165);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(102, 86);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "B";
+            this.pictureBox4.Click += new System.EventHandler(this.Clique);
+            // 
+            // lblPontos
+            // 
+            this.lblPontos.AutoSize = true;
+            this.lblPontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPontos.ForeColor = System.Drawing.Color.White;
+            this.lblPontos.Location = new System.Drawing.Point(44, 290);
+            this.lblPontos.Name = "lblPontos";
+            this.lblPontos.Size = new System.Drawing.Size(101, 29);
+            this.lblPontos.TabIndex = 4;
+            this.lblPontos.Text = "Pontos:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(375, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            this.iniciarToolStripMenuItem.Click += new System.EventHandler(this.Opcoes);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            this.sobreToolStripMenuItem.Click += new System.EventHandler(this.Opcoes);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem,
+            this.sobreToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(59, 20);
+            this.toolStripMenuItem1.Text = "Opções";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.Opcoes);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.Opcoes);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(375, 352);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblPontos);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Genius";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lblPontos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
---
+++ JogoGenius/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace JogoGenius
+{
+    public partial class Form1 : Form
+    {
+        List<string> sequenciaCores = new List<string>();
+        List<string> sequenciaJogador = new List<string>();
+        int listaIndex, pontos;
+        const float brilho = 0.7f;
+        string atualCor;
+        string[] cores = { "R", "G", "B", "Y" };
+        bool podeJogar;
+        Random rdn = new Random();
+        SoundPlayer[] audios = { new SoundPlayer(Properties.Resources.beep_01), new SoundPlayer(Properties.Resources.beep_02), new SoundPlayer(Properties.Resources.beep_03), new SoundPlayer(Properties.Resources.beep_04) };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarCor(PictureBox pic, Color corClara, Color corEscura, SoundPlayer som)
+        {
+            pic.BackColor = corClara; som.Play(); Application.DoEvents(); Thread.Sleep(850); pic.BackColor = corEscura;
+        }
+
+        private void ProcurarCor(string tagPic)
+        {
+            foreach(var corEncontrada in Controls.OfType<PictureBox>())
+            {
+                if(corEncontrada.Tag.ToString() == tagPic)
+                {
+                    string tag = corEncontrada.Tag.ToString();
+
+                    Color piscar = ControlPaint.Light(corEncontrada.BackColor, brilho);
+                    SoundPlayer som = tag == "R" ? audios[0] : (tag == "G" ? audios[1] : (tag == "B" ? audios[2] : audios[3]));
+                    MostrarCor(corEncontrada, piscar, corEncontrada.BackColor, som);
+                }
+            }
+        }
+
+        private void SortearCor()
+        {
+            atualCor = cores[rdn.Next(0, cores.Length)];
+            sequenciaCores.Add(atualCor);
+
+            foreach(var cor in sequenciaCores)
+            {
+                Application.DoEvents(); Thread.Sleep(250);
+                ProcurarCor(cor);
+            }
+
+            podeJogar = true;
+        }
+
+        private void Clique (object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+
+            if (podeJogar)
+            {
+                podeJogar = false;
+                atualCor = pb.Tag.ToString();
+                sequenciaJogador.Add(atualCor);
+                ProcurarCor(atualCor);
+
+                if(sequenciaJogador[listaIndex] == sequenciaCores[listaIndex])
+                {
+                    pontos++; lblPontos.Text = "Pontos: " + pontos.ToString(); listaIndex++; Checar();
+                }
+                else
+                {
+                    MessageBox.Show("Sequencia Incorreta ! Fim de jogo!");
+                }
+            }
+        }
+
+        private void Checar()
+        {
+            if (listaIndex == sequenciaCores.Count)
+            {
+                listaIndex = 0; sequenciaJogador.Clear(); SortearCor();
+            }
+            else
+            {
+                podeJogar = true;
+            }
+
+        }
+
+        private void Opcoes(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menu = (ToolStripMenuItem)sender;
+
+            switch (menu.Text)
+            {
+                case "Iniciar": pontos = 0; lblPontos.Text = "Pontos:"; sequenciaJogador.Clear(); sequenciaCores.Clear();
+                    podeJogar = false; listaIndex = 0; SortearCor(); break;
+
+                case "Sobre": string info = "Nome do Jogo: Genius \n Desenvolvedor: Marco Antonio Pandolfo \n Data: 2018";
+                    MessageBox.Show(info); break;
+
+                case "Sair": Application.Exit(); break;
+            }
+        }
+    }
+}
